b87d3cfe4744af8853da3f370e487aef998b6f09
Peter Homberg <peter.homberg@durr.com>
2026-04-05 18:17:23 +0800

    Add ios XCode skeleton

---
+++ ios/WateringSystem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WateringSystem
+//
+//  Created by Peter Homberg on 4/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/WateringSystem/WateringSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  WateringSystemApp.swift
+//  WateringSystem
+//
+//  Created by Peter Homberg on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WateringSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
